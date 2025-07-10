a71e65a8022e6348660450b6a068caa3313b2396
GlassyBridge114 <leul2027@hmacademy.org>
2025-07-10 20:05:57 +0300

    The basic version now works

--- script.js
+++ script.js
@@ -2,11 +2,9 @@ const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
-console.log(ctx);
 
 const image = new Image();
 image.src = 'Screenshot 2025-07-10 at 5.09.11 PM.png';
-console.log(image);
 
 class Logo {
     constructor(effect, image){
@@ -17,22 +15,44 @@ class Logo {
         this.height = this.image.naturalHeight;
         this.x = Math.random() * (this.effect.width - this.width);
         this.y = Math.random() * (this.effect.height - this.height);
+        
+        this.vx = 3;
+        this.vy = 3;
     }
     draw(context){
         context.drawImage(this.image, this.x, this.y, this.width, this.height);
     }
-}
-
-class Effect {
-    constructor(canvas){
-        this.canvas = canvas;
-        this.width = this.canvas.width;
-        this.height = this.canvas.height;
+    update(){
+        this.x += this.vx;
+        if (this.x > this.effect.width - this.width|| this.x < 0) this.vx *= -1;
+        this.y += this.vy;
+        if (this.y > this.effect.height - this.height|| this.y < 0) this.vy *= -1;
     }
 }
 
-image.onload = () => {
-    const effect = new Effect(canvas);
-    const logo = new Logo(effect, image);
-    logo.draw(ctx);
-};
+class Effect {
+    constructor(canvas, image){
+        this.canvas = canvas;
+        this.width = this.canvas.width;
+        this.height = this.canvas.height;
+        this.image = image;
+
+        this.image.onload = () => {
+            this.logo = new Logo(this, this.image);
+        };
+        this.image.onload();
+    }
+    handleImage(context){
+        this.logo.draw(context);
+        this.logo.update();
+    }
+}
+const effect = new Effect(canvas, image);
+
+function animate() {
+    ctx.clearRect(0,0, canvas.width, canvas.height);
+    effect.handleImage(ctx);
+    requestAnimationFrame(animate);
+}
+
+animate();
