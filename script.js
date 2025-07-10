23cf7dcb41709d9a8080f005ee8049705f6d754e
GlassyBridge114 <leul2027@hmacademy.org>
2025-07-10 21:38:23 +0300

    Made the size adjustable and added the DVD logo, also added hue shift

--- script.js
+++ script.js
@@ -1,26 +1,55 @@
 const canvas = document.getElementById('canvas1');
-const ctx = canvas.getContext('2d');
+const ctx = canvas.getContext('2d', {willReadFrequently: true});
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 const image = new Image();
-image.src = 'Screenshot 2025-07-10 at 5.09.11 PM.png';
+image.src = 'https://www.pngfind.com/pngs/b/47-476875_dvd-logo-png.png';
+//image.src = 'Screenshot 2025-07-10 at 5.09.11 PM.png';
+
+image.onload = function (){
+    const width = image.naturalWidth;
+    const height = image.naturalHeight;
+
+    const ratio = width/height;
+};
 
 class Logo {
     constructor(effect, image){
         this.effect = effect;
         this.image = image;
-
-        this.width = this.image.naturalWidth;
-        this.height = this.image.naturalHeight;
+        this.size = 150;
+        
+        let {width,height} = this.ratio(this.image);
+        this.width = this.size * width;
+        this.height = this.size * height;
+        
         this.x = Math.random() * (this.effect.width - this.width);
         this.y = Math.random() * (this.effect.height - this.height);
         
         this.vx = 3;
         this.vy = 3;
+        
+    }
+    ratio(image){
+        let width = image.naturalWidth;
+        let height = image.naturalHeight;
+        const aspectRatio = width / height;
+            
+        width = 1;
+        height = 1 / aspectRatio;
+
+        return {width, height};
     }
     draw(context){
         context.drawImage(this.image, this.x, this.y, this.width, this.height);
+
+        context.save();
+        context.globalCompositeOperation = 'color'; //'source-atop' for transparent backgrounds.
+        context.fillStyle = 'hsl(' + this.x / 4 + ', 100%, 50%)';
+
+        context.fillRect(this.x, this.y, this.width, this.height);
+        context.restore();
     }
     update(){
         this.x += this.vx;
@@ -40,6 +69,7 @@ class Effect {
         this.image.onload = () => {
             this.logo = new Logo(this, this.image);
         };
+
         this.image.onload();
     }
     handleImage(context){
