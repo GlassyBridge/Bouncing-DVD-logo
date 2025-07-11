f93f8fbf1baa65b612799e767f1b7f94486dbe24
GlassyBridge114 <leul2027@hmacademy.org>
2025-07-11 11:18:54 +0300

    Added controls And reworked the Logo and Effect class to bette work together with the image

--- script.js
+++ script.js
@@ -1,39 +1,81 @@
-const canvas = document.getElementById('canvas1');
-const ctx = canvas.getContext('2d', {willReadFrequently: true});
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-
-const image = new Image();
-image.src = 'https://www.pngfind.com/pngs/b/47-476875_dvd-logo-png.png';
-//image.src = 'Screenshot 2025-07-10 at 5.09.11 PM.png';
-
-image.onload = function (){
-    const width = image.naturalWidth;
-    const height = image.naturalHeight;
-
-    const ratio = width/height;
-};
-
 class Logo {
-    constructor(effect, image){
+    constructor(effect){
         this.effect = effect;
-        this.image = image;
-        this.size = 150;
+        this._size = 150;
+        this._speed = 3;
+
+        this.image = new Image();
+        this.image.src = 'dvd-logo-png-19264.png';
         
-        let {width,height} = this.ratio(this.image);
-        this.width = this.size * width;
-        this.height = this.size * height;
+        this.image.onload = () => {
+            console.log("Image Loaded");
+            
+            let {width,height} = this.ratio(this.image);
+            this.width = this.size * width;
+            this.height = this.size * height;
+            
+            this.x = Math.random() * (this.effect.width - this.width);
+            this.y = Math.random() * (this.effect.height - this.height);
+        };
+
+        this.vx = this.speed;
+        this.vy = this.speed;
+
+        this.image.onload();
         
-        this.x = Math.random() * (this.effect.width - this.width);
-        this.y = Math.random() * (this.effect.height - this.height);
-        
-        this.vx = 3;
-        this.vy = 3;
-        
+        this.type = 'color';
+        this.color = 'hsl(' + this.x / 4 + ', 100%, 50%)';
+        this.change = true;
     }
+
+    set size(newSize) {
+        if (this._size !== newSize) {
+            this._size = newSize;
+
+            let {width,height} = this.ratio(this.image);
+            this.width = this.size * width;
+            this.height = this.size * height;
+
+            this.x = Math.max(0, Math.min(this.effect.width - this.width, this.x));
+            this.y = Math.max(0, Math.min(this.effect.height - this.height, this.y));
+        }
+    }
+    get size() {
+        return this._size;
+    }
+
+    set speed(newSpeed) {
+        if (this._speed !== newSpeed) {
+            this._speed = newSpeed;
+
+            this.vx = (this.vx > 0 ? 1 : -1) * this._speed;
+            this.vy = (this.vy > 0 ? 1 : -1) * this._speed;
+        }
+    }
+    get speed() {
+        return this._speed;
+    }
+
+    set imageSrc(newSrc) {
+        if (this.image.src !== newSrc) {
+            this.image.src = newSrc;
+
+            let {width,height} = this.ratio(this.image);
+            this.width = this.size * width;
+            this.height = this.size * height;
+        }
+    }
+    get imageSrc() {
+        return this.image.src;
+    }
+
     ratio(image){
         let width = image.naturalWidth;
         let height = image.naturalHeight;
+
+        if (width == 0 || height == 0){
+            return {width : 1, height : 1};
+        }
         const aspectRatio = width / height;
             
         width = 1;
@@ -45,8 +87,14 @@ class Logo {
         context.drawImage(this.image, this.x, this.y, this.width, this.height);
 
         context.save();
-        context.globalCompositeOperation = 'source-atop'; //'source-atop' for transparent backgrounds else use 'color'/'multiply'
-        context.fillStyle = 'hsl(' + this.x / 4 + ', 100%, 50%)';
+        context.globalCompositeOperation = this.type; //'source-atop' for transparent backgrounds else use 'color'/'multiply'
+        
+        if (this.change) {
+            context.fillStyle = 'hsl(' + this.x / 4 + ', 100%, 50%)';
+        }
+        else {
+            context.fillStyle = this.color;
+        }
 
         context.fillRect(this.x, this.y, this.width, this.height);
         context.restore();
@@ -60,24 +108,36 @@ class Logo {
 }
 
 class Effect {
-    constructor(canvas, image){
+    constructor(canvas){
         this.canvas = canvas;
         this.width = this.canvas.width;
         this.height = this.canvas.height;
-        this.image = image;
 
-        this.image.onload = () => {
-            this.logo = new Logo(this, this.image);
-        };
-
-        this.image.onload();
+        this.logo = new Logo(this);
+    }
+    changeSpeed(speed){
+        this.logo.speed = speed;
+    }
+    changeSize(size){
+        this.logo.size = size;
+    }
+    changeImage(imageUrl){
+        this.logo.image.src = imageUrl;
+    }
+    changeType(type){
+        this.logo.type = type;
+    }
+    changeColor(color){
+        this.logo.color = color;
+    }
+    randomColor(bolean){
+        this.logo.change = bolean;
     }
     handleImage(context){
         this.logo.draw(context);
         this.logo.update();
     }
 }
-const effect = new Effect(canvas, image);
 
 function animate() {
     ctx.clearRect(0,0, canvas.width, canvas.height);
@@ -85,4 +145,80 @@ function animate() {
     requestAnimationFrame(animate);
 }
 
+const canvas = document.getElementById('canvas1');
+const ctx = canvas.getContext('2d', {willReadFrequently: true});
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+const effect = new Effect(canvas);
+
 animate();
+
+const speedInput = document.getElementById('speedInput');
+speedInput.value = effect.logo.speed;
+const speedDisplay = document.getElementById('currentSpeed');
+speedDisplay.value = effect.logo.speed;
+
+const sizeInput = document.getElementById('sizeInput');
+sizeInput.value = effect.logo.size / 10;
+const sizeDisplay = document.getElementById('currentSize');
+sizeDisplay.value = effect.logo.size / 10;
+
+const randomColor = document.getElementById('rd-color');
+const wiotb = document.getElementById('image-check');
+
+const image = document.getElementById('image');
+const color = document.getElementById('color');
+const colorCont = document.getElementById('color-field')
+
+speedInput.addEventListener('input', () => {
+    speedDisplay.value = speedInput.value;
+    effect.changeSpeed(parseInt(speedInput.value, 10));
+});
+
+speedDisplay.addEventListener('input', () => {
+    speedInput.value = speedDisplay.value;
+    effect.changeSpeed(parseInt(speedDisplay.value, 10));
+});
+
+sizeInput.addEventListener('input', () => {
+    sizeDisplay.value = sizeInput.value;
+    effect.changeSize(parseInt(sizeInput.value, 10) * 10);
+});
+
+sizeDisplay.addEventListener('input', () => {
+    sizeInput.value = sizeDisplay.value;
+    effect.changeSize(parseInt(sizeDisplay.value, 10) * 10);
+});
+
+image.addEventListener('change', function(event) {
+    const file = event.target.files[0];
+    if (file) {
+        effect.changeImage(URL.createObjectURL(file));
+    }
+});
+
+wiotb.addEventListener('input', () => {
+    if (wiotb.checked){
+        effect.changeType('source-atop');
+    }
+    else {
+        effect.changeType('multiply');
+    }
+});
+
+randomColor.addEventListener('input', () => {
+    if (randomColor.checked){
+        colorCont.hidden = true;
+        effect.randomColor(true);
+    }
+    else {
+        colorCont.hidden = false;
+        effect.randomColor(false);
+    }
+});
+
+color.addEventListener('input', () => {
+    console.log('color: ' + color.value);
+    effect.changeColor(color.value);
+});
